2fafc1f94cbe24aa438809719ebe0ad805b5f419
VISHNU KUMAWAT <115189913+VISHNU4546@users.noreply.github.com>
2024-10-18 16:21:53 +0530

    Update middleware.js

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -8,7 +8,7 @@ module.exports.isLoggedIn = (req,res,next)=>{
     if(!req.isAuthenticated()){
         req.session.redirectUrl = req.originalUrl;
         req.flash("error","Please login...!");
-       return res.redirect("/login");
+       return res.redirect("/login"); 
     }
     next();
 }
