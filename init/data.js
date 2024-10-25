2994efb75ca88acbe5f0867ff1c7ee671b7fd59b
VISHNU KUMAWAT <115189913+VISHNU4546@users.noreply.github.com>
2024-10-25 09:58:04 +0530

    Update data.js

--- init/data.js
+++ init/data.js
@@ -6,7 +6,7 @@ const sampleListings = [
     image:"https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
     price: 1500,
     location: "Malibu",
-    country: "United States",
+    country: "United States", 
     geometry: { type: 'Point', coordinates: [-118.6923, 34.0381] },
   },
   {
